88c9f4dae7e4d3eb1d65934d84b3316f4edca9e3
bdidukh <bdidukh@tsukat.com>
2024-03-13 23:23:20 +0200

    Add google sign-in button

--- app/src/main/java/com/spm/vasylyshyn/counterdisplayapp/activity/LoginActivity.kt
+++ app/src/main/java/com/spm/vasylyshyn/counterdisplayapp/activity/LoginActivity.kt
@@ -7,9 +7,13 @@ import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
-import com.spm.vasylyshyn.counterdisplayapp.response.JWTTokenSuccessResponse
-import com.spm.vasylyshyn.counterdisplayapp.R
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.tasks.Task
 import com.spm.vasylyshyn.counterdisplayapp.API_URL_PATH
+import com.spm.vasylyshyn.counterdisplayapp.R
+import com.spm.vasylyshyn.counterdisplayapp.response.JWTTokenSuccessResponse
 import com.spm.vasylyshyn.counterdisplayapp.response.LoginRequest
 import com.spm.vasylyshyn.counterdisplayapp.service.AuthService
 import retrofit2.Call
@@ -24,6 +28,7 @@ class LoginActivity : AppCompatActivity() {
         setContentView(R.layout.activity_login)
         val login_btn: Button = findViewById(R.id.login_btn)
         val reg_btn:Button = findViewById(R.id.reg_btn)
+        val login_google_btn: Button = findViewById(R.id.google_login_btn)
         val password: EditText = findViewById(R.id.passwordEditText)
         val login: EditText = findViewById(R.id.logibEditText)
         login_btn.setOnClickListener {
@@ -63,7 +68,34 @@ class LoginActivity : AppCompatActivity() {
             val intent = Intent(applicationContext, RegisterActivity::class.java)
             startActivity(intent)
         }
+        login_google_btn.setOnClickListener {
+            val gso: GoogleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                // TODO: add .requestIdToken("MyToken")
+                .requestEmail()
+                .build()
+            val mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+            val signInIntent = mGoogleSignInClient.signInIntent
+            startActivityForResult(signInIntent, 9001)
+        }
     }
+
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (data == null) return
+        if (requestCode == 9001) {
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+
+            task.addOnSuccessListener {
+                // TODO: Perform the sign-in action
+                val it1: GoogleSignInAccount = it
+                val intent = Intent(applicationContext, MainActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener { exception ->
+                Log.w("Error getting documents: ", exception)
+            }
+        }
+    }
+
     companion object {
         var token:String = ""
     }
